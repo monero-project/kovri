7e329432692f314ea031d69bfe1ed9f5dc3bdb94
EinMByte <einmbyte@i2pmail.org>
2016-09-13 13:05:37 +0200

    Some Transports refactoring.
    Added documentation, simplified some code.
    Relates to #348.

--- src/core/transport/transports.h
+++ src/core/transport/transports.h
@@ -43,7 +43,6 @@
 #include <map>
 #include <memory>
 #include <mutex>
-#include <ostream>
 #include <queue>
 #include <string>
 #include <thread>
@@ -64,6 +63,9 @@
 namespace i2p {
 namespace transport {
 
+/// @class DHKeysPairSupplier
+/// @brief Pregenerates Diffie-Hellman key pairs for use in key exchange
+/// TODO(unassigned): Does this really need to run on a separate thread?
 class DHKeysPairSupplier {
  public:
   DHKeysPairSupplier(
@@ -95,53 +97,81 @@ class DHKeysPairSupplier {
   std::mutex m_AcquiredMutex;
 };
 
+/// @class Peer
+/// @brief Stores information about transport peers.
 struct Peer {
   std::size_t num_attempts;
   std::shared_ptr<const i2p::data::RouterInfo> router;
   std::list<std::shared_ptr<TransportSession>> sessions;
   std::uint64_t creation_time;
   std::vector<std::shared_ptr<i2p::I2NPMessage>> delayed_messages;
-  void Done() {
-    for (auto it : sessions)
-      it->Done();
-  }
+
+  void Done();
 };
 
 const std::size_t SESSION_CREATION_TIMEOUT = 10;  // in seconds
-const std::uint32_t LOW_BANDWIDTH_LIMIT = 32 * 1024;  // 32KBs
+const std::uint32_t LOW_BANDWIDTH_LIMIT = 32 * 1024;  // 32KBps
 
+/// @class Transports
+/// @brief Provides functions to pass messages to a given peer.
+///        Manages the SSU and NTCP transports.
 class Transports {
  public:
   Transports();
+
   ~Transports();
 
+  /// @brief Starts SSU and NTCP server instances, as well as the cleanup timer.
+  ///        If enabled, the UPnP service is also started.
   void Start();
 
+  /// @brief Stops all services ran by this Transports object.
   void Stop();
 
+  /// @return a reference to the boost::asio::io_serivce that is used by this
+  ///         Transports object
   boost::asio::io_service& GetService() {
     return m_Service;
   }
 
+  /// @return a pointer to a Diffie-Hellman pair
   std::unique_ptr<i2p::transport::DHKeysPair> GetNextDHKeysPair();
 
+  /// @brief Returns a keypair for reuse.
+  // TODO(unassigned): Do not reuse ephemeral keys, the whole point is that they
+  //                   are not reused.
   void ReuseDHKeysPair(
       std::unique_ptr<DHKeysPair> pair);
 
+  /// @brief Asynchronously sends a message to a peer.
+  /// @param ident the router hash of the remote peer
+  /// @param msg the I2NP message to deliver
   void SendMessage(
       const i2p::data::IdentHash& ident,
       std::shared_ptr<i2p::I2NPMessage> msg);
 
+  /// @brief Asynchronously sends one or more messages to a peer.
+  /// @param ident the router hash of the remote peer
+  /// @param msgs the I2NP messages to deliver
   void SendMessages(
       const i2p::data::IdentHash& ident,
       const std::vector<std::shared_ptr<i2p::I2NPMessage>>& msgs);
 
+  /// @brief Asynchronously close all transport sessions to the given router.
+  /// @param router the i2p::data::RouterInfo of the router to disconnect from
+  /// @note if router is nullptr, nothing happens
   void CloseSession(
       std::shared_ptr<const i2p::data::RouterInfo> router);
 
+  /// @brief Informs this Transports object that a new peer has connected
+  ///        to us
+  /// @param session the new session
   void PeerConnected(
       std::shared_ptr<TransportSession> session);
 
+  /// @brief Informs this Transports object that a peer has disconnected
+  ///        from us
+  /// @param session the session that has ended
   void PeerDisconnected(
       std::shared_ptr<TransportSession> session);
 
@@ -185,15 +215,8 @@ class Transports {
   std::shared_ptr<const i2p::data::RouterInfo> GetRandomPeer() const;
 
   /// @return Log-formatted string of session info
-  const std::string GetFormattedSessionInfo(
-      std::shared_ptr<const i2p::data::RouterInfo>& router) {
-    if (router) {
-      std::ostringstream info;
-      info << " [" << router->GetIdentHashAbbreviation() << "] ";
-      return info.str();
-    }
-    return "[hash unavailable]";
-  }
+  std::string GetFormattedSessionInfo(
+      std::shared_ptr<const i2p::data::RouterInfo>& router) const;
 
  private:
   void Run();
@@ -215,6 +238,11 @@ class Transports {
 
   bool ConnectToPeer(
       const i2p::data::IdentHash& ident, Peer& peer);
+
+  bool ConnectToPeerNTCP(
+      const i2p::data::IdentHash& ident, Peer& peer);
+
+  bool ConnectToPeerSSU(Peer& peer);
 
   void HandlePeerCleanupTimer(
       const boost::system::error_code& ecode);

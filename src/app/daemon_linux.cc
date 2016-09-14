9d547400c2e9f190fc5a7fb3f91bddc534541d83
guzzi_jones <guzzijones12@gmail.com>
2016-09-14 22:42:06 +0000

    COVERITY :: see #263 ; cid135926

--- src/app/daemon_linux.cc
+++ src/app/daemon_linux.cc
@@ -90,11 +90,14 @@ bool DaemonLinux::Start() {
     chdir(d.c_str());
     // close stdin/stdout/stderr descriptors
     ::close(0);
-    ::open("/dev/null", O_RDWR);
+    if(::open("/dev/null", O_RDWR)<0)
+	    exit(1);
     ::close(1);
-    ::open("/dev/null", O_RDWR);
+    if(::open("/dev/null", O_RDWR)<0)
+	    exit(1);
     ::close(2);
-    ::open("/dev/null", O_RDWR);
+    if(::open("/dev/null", O_RDWR)<0)
+	    exit(1);
   }
   // Pidfile
   m_pidFile = (i2p::util::filesystem::GetDataPath() / "kovri.pid").string();

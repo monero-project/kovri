7c760573c9410f83e13bcc556bd12df9b25a17e7
anonimal <anonimal@i2pmail.org>
2016-09-15 14:51:03 +0000

    HTTP: improve 150a6a5 for CID 146759. Refs #357
    References #263

--- src/client/util/http.cc
+++ src/client/util/http.cc
@@ -345,21 +345,26 @@ void HTTP::MergeI2PChunkedResponse(
   while (std::getline(response, hex)) {
     std::istringstream hex_len(hex);
     // Convert to integer value
-    std::size_t len;
+    std::size_t len(0);
     // CID 146759 complains of TAINTED_SCALAR but we're guaranteed a useful value because:
     // 1. The HTTP response is chunked and prepends hex value of chunk size
     // 2. If length is null, we'll break before new buffer
     // Note: verifying the validity of stated chunk size against ensuing content size
     // will require more code (so, better to complete cpp-netlib refactor instead)
-    hex_len >> std::hex >> len;
-    // If last chunk, break
-    if (!len)
+    if (!(hex_len >> std::hex >> len).fail()) {
+      // If last chunk, break
+      if (!len)
+        break;
+      // Read in chunk content of chunk size
+      auto buf = std::make_unique<char[]>(len);
+      response.read(buf.get(), len);
+      merged.write(buf.get(), len);
+      std::getline(response, hex);  // read \r\n after chunk
+    } else {
+      LogPrint(eLogError,
+          "HTTP: stream error, unable to read line from chunked response");
       break;
-    // Read in chunk content of chunk size
-    auto buf = std::make_unique<char[]>(len);
-    response.read(buf.get(), len);
-    merged.write(buf.get(), len);
-    std::getline(response, hex);  // read \r\n after chunk
+    }
   }
 }
 

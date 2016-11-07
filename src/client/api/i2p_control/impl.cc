8dac64130faec1e69ba4c24dc3ef8f74cb908c0d
anonimal <anonimal@i2pmail.org>
2016-11-07 04:31:26 +0000

    I2PControl: remove deprecations, add debug logging
    - Remove cryptopp headers
    - Remove boost version check
    - Add minor debug logging in request handler
    
    Noticed when closing #377

--- src/client/api/i2p_control/impl.cc
+++ src/client/api/i2p_control/impl.cc
@@ -34,9 +34,6 @@
 
 #include <boost/property_tree/json_parser.hpp>
 
-#include <cryptopp/filters.h>
-#include <cryptopp/hex.h>
-
 #include <iomanip>
 #include <sstream>
 #include <string>
@@ -298,6 +295,7 @@ void I2PControlSession::Stop() {
 I2PControlSession::Response I2PControlSession::HandleRequest(
     std::stringstream& request) {
   boost::property_tree::ptree pt;
+  LogPrint(eLogDebug, "I2PControlSession: reading json request");
   boost::property_tree::read_json(request, pt);
   Response response;
   try {
@@ -317,7 +315,7 @@ I2PControlSession::Response I2PControlSession::HandleRequest(
           "I2PControlSession: invalid token presented");
       return response;
     }
-    // Call the appropriate handler
+    LogPrint(eLogDebug, "I2PControlSession: calling handler");
     (this->*(it->second))(params, response);
   } catch (const boost::property_tree::ptree_error&) {
     response.SetError(ErrorCode::e_ParseError);

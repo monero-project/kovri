403519bc6356e890f5ec38a84d34adfb4ebbb85e
anonimal <anonimal@i2pmail.org>
2016-12-11 19:40:54 +0000

    Instance: move ctor impl out of header. Refs #494
    Better practice but not directly related to #494 fix.

--- src/app/instance.h
+++ src/app/instance.h
@@ -46,10 +46,10 @@ namespace app {
 class Instance {
  public:
    // TODO(unassigned): see note and TODO in main about multiple instances
-   Instance(
-       std::vector<std::string>& args)
-       : m_Config(args),
-         m_IsReloading(false) {}
+   explicit Instance(
+       std::vector<std::string>& args);
+
+   ~Instance();
 
   /// @brief Configures instance
   void Configure();

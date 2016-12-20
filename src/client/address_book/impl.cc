monero-project/kovri

commit 5062d709bdcabf1a9edfd66b51db356a479d1389

AddressBook: hardening + unit-test + refactoring
References #305

src/client/address_book/impl.cc
@@ -318,6 +318,7 @@ bool AddressBook::SaveSubscription(
         file << address.first << '\n';
         // Add to address book
         m_Storage->AddAddress(address.second);
+        m_Addresses[address.first] = address.second.GetIdentHash();  // TODO(anonimal): setter?
       }
       // Flush subscription file
       file << std::flush;
@@ -335,19 +336,24 @@ bool AddressBook::SaveSubscription(
 
 // TODO(anonimal): unit-test
 
-const std::vector<std::pair<std::string, kovri::core::IdentityEx>>
+const std::map<std::string, kovri::core::IdentityEx>
 AddressBook::ValidateSubscription(std::istream& stream) {
   LogPrint(eLogDebug, "AddressBook: validating subscription");
-  std::vector<std::pair<std::string, kovri::core::IdentityEx>> addresses;
-  std::string hostname;  // Paired to address identity
+  // Map host to address identity
+  std::map<std::string, kovri::core::IdentityEx> addresses;
+  // To ensure valid hostname
+  std::string hostname;
+  // Note: do not rely on [a-z] to catch all letters as it may fail on some locales
+  const std::string alpha = "abcdefghijklmnopqrstuvwxyz";
+  // TODO(unassigned): expand when we want to venture beyond the .i2p TLD
+  std::regex regex("((?:[-"+alpha+"0-9]+.)+["+alpha+"|(i2p)]{2,})");
   try {
     // Read through stream, add to address book
     while (std::getline(stream, hostname)) {
-      // TODO(anonimal): Boost refactor
-      // TODO(anonimal): allow GPG clearsigned subscriptions?
-      // Skip empty line
-      if (hostname.empty())
+      // Skip empty / too large lines
+      if (hostname.empty() || hostname.size() > AddressBookSize::SubscriptionLine)
         continue;
+      // TODO(anonimal): Boost refactor
       // Clear whitespace before and after host (on the line)
       hostname.erase(
           std::remove_if(
@@ -356,21 +362,20 @@ AddressBook::ValidateSubscription(std::istream& stream) {
               [](char whitespace) { return std::isspace(whitespace); }),
           hostname.end());
       // Parse Hostname=Base64Address from line
-      kovri::core::IdentityEx ident;  // For host ident hash (from Base64 address)
+      kovri::core::IdentityEx ident;
       std::size_t pos = hostname.find('=');
       if (pos != std::string::npos) {
         const std::string name = hostname.substr(0, pos++);
         const std::string addr = hostname.substr(pos);
-        // TODO(anonimal):  ensure only valid hostname (and missing hostname)
-        if (!ident.FromBase64(addr)) {
-          LogPrint(eLogError,
-              "AddressBook: malformed address ", addr, " for ", name);
+        // Ensure only valid Hostname=Base64Address
+        if (name.empty() || addr.empty()
+            || !std::regex_search(name, regex)
+            || !ident.FromBase64(addr)) {
+          LogPrint(eLogWarn, "AddressBook: malformed address, skipping");
           continue;
         }
-        m_Addresses[name] = ident.GetIdentHash();  // TODO(anonimal): setter?
+        addresses[hostname] = ident;  // Host is valid, save
       }
-      // TODO(anonimal): more hardening?
-      addresses.push_back(std::make_pair(hostname, ident));  // Host is valid, save
     }
   } catch (const std::exception& ex) {
     LogPrint(eLogError, "AddressBook: exception during validation: ", ex.what());

tests/unit_tests/client/address_book/impl.cc
@@ -1,0 +1,164 @@
+/**                                                                                           //
+ * Copyright (c) 2015-2016, The Kovri I2P Router Project                                      //
+ *                                                                                            //
+ * All rights reserved.                                                                       //
+ *                                                                                            //
+ * Redistribution and use in source and binary forms, with or without modification, are       //
+ * permitted provided that the following conditions are met:                                  //
+ *                                                                                            //
+ * 1. Redistributions of source code must retain the above copyright notice, this list of     //
+ *    conditions and the following disclaimer.                                                //
+ *                                                                                            //
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list     //
+ *    of conditions and the following disclaimer in the documentation and/or other            //
+ *    materials provided with the distribution.                                               //
+ *                                                                                            //
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be       //
+ *    used to endorse or promote products derived from this software without specific         //
+ *    prior written permission.                                                               //
+ *                                                                                            //
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY        //
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    //
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL     //
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,       //
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,               //
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    //
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,          //
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF    //
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               //
+ */
+
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+#include <cstdint>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "client/address_book/impl.h"
+
+#include "core/crypto/rand.h"
+
+BOOST_AUTO_TEST_SUITE(AddressBook);
+
+/// @class SubscriptionFixture
+struct SubscriptionFixture {
+
+  /// @brief Validates given lines as proven addressbook host/address pairs
+  /// @param lines Lines to validate
+  /// @return Only valid data that was parsed
+  const auto Validate(const std::vector<std::string>& lines) {
+    std::stringstream stream;
+    for (auto const& l : lines)
+      stream << l << "\n";
+    return book.ValidateSubscription(stream);
+  }
+
+  /// @brief Compares and validates lines against subscription fixture
+  /// @return False if test data does not match subscription fixture
+  bool Validate() {
+    // Save fixture (malformed or redundant lines will be removed)
+    for (auto const& s : subscription) {
+      const std::string valid(s);
+      lines.push_back(valid);
+    }
+    // Validate
+    std::vector<std::string> validated;
+    for (auto const& v : Validate(lines))
+      validated.push_back(v.first);
+    return validated != subscription ? false : true;
+  }
+
+  /// @brief Subscription with valid hosts
+  const std::vector<std::string> subscription {{
+    "anonimal.i2p=AQZGLAMpI9Q0l0kmMj1vpJJYK3CjLp~fE3MfvE-e7KMKjI5cPOH6EN8m794uHJ6b09qM8mb9VEv1lVLEov~usVliTSXCSHuRBOCIwIOuDNU0AbVa4BpIx~2sU4TxKhoaA3zQ6VzINoduTdR2IJhPvI5xzezp7dR21CEQGGTbenDslXeQ4iLHFA2~bzp1f7etSl9T2W9RID-KH78sRQmzWnv7dbhNodMbpO6xsf1vENf6bMRzqD5vgHEHZu2aSoNuPyYxDU1eM6--61b2xp9mt1k3ud-5WvPVg89RaU9ugU5cxaHgR927lHMCAEU2Ax~zUb3DbrvgQBOTHnJEx2Fp7pOK~PnP6ylkYKQMfLROosLDXinxOoSKP0UYCh2WgIUPwE7WzJH3PiJVF0~WZ1dZ9mg00c~gzLgmkOxe1NpFRNg6XzoARivNVB5NuWqNxr5WKWMLBGQ9YHvHO1OHhUJTowb9X90BhtHnLK2AHwO6fV-iHWxRJyDabhSMj1kuYpVUBQAEAAcAAA==",
+    "kovri.i2p=0UVPqAA4xUSfPYPBca24h8fdokhwcJZ4zWvELv-5OsBYTHKtnLzvK7byXtXT~fOV2pExi8vrkgarGTNDfJbB2KCsdVS3V7qwtTvoCGYyklcDBlJsWMj7H763hEz5rt9SzLkcpwhO3t0Zwe6jXL1UB-QW8KxM30t-ZOfPc6OiJ1QpnE6Bo5OUm6jPurQGXdWCAPio5Z-YnRL46n0IHWOQPYYSStJMYPlPS-S75rMIKbZbEMDraRvSzYAphUaHfvtWr2rCSPkKh3EbrOiBYiAP2oWvAQCsjouPgVF2qwQRnBbiAezHedM2gXzkgIyCV2kGOOcHhiihd~7fWwJOloH-gO78QkmCuY-3kp3633v3MBw7pmABr-XNKWnATZOuf2syWVBZbTnOXsWf41tu6a33HOuNsMxAOUrwbu7QRmT4X8X-EITwNlKN6r1t3uoQ~yZm4RKsJUsBGfVtKl8PBMak3flQAg95oV0OBDGuizIQ9vREOWvPGlQCAXZzEg~cUNbfBQAEAAcAAA==",
+    "monero.i2p=3VzGaQQXwzN1iAwaPI17RK~gUqKqMH6fI2dkkGBwdayAPAdiZMyk1KGoTq~q1~HBraPZnz9mZJlzf6WVGCkUmUV3SBjBEbrdL9ud0fArq3P1~Ui9ViR9B7m5EG8smAnFvKZdqS-cnmHploUfIOefoQe0ecM7YYHErZsn3kL-WtvlfoDiSth-edIBpWxeHfmXSKoHSGSJ2snl5p9hxh30KmKj9AB0d4En-jcD83Ep3jsSvtPoQl7tSsh575~q0JJLsqGqm2sR9w4nZr7O58cg-21A2tlZeldM287uoTMb9eHWnYuozUGzzWOXvqg0UxPQSTfwh7YEhx0aRTXT2OFpr84XPoH2M6xIXfEMkFtJEJ-XlM-ILUZkg3kuBEFN7n4mBK~8L0Ht1QCq8L3~y7YnN61sxC0E9ZdyEOoC~nFJxndri9s9NzgZPo5eo6DsZXweOrTAIVQgKFUozL7WXKMlgqBZ5Nl3ijD6MGCIy0fWYHGLJ4jDBY7wrcfynVXFGm4EBQAEAAcAAA=="
+  }};
+
+  /// @brief Test data to verify against Host=Address
+  std::vector<std::string> lines;
+
+  /// @brief Addressbook instance
+  kovri::client::AddressBook book;
+};
+
+BOOST_FIXTURE_TEST_SUITE(AddressBook, SubscriptionFixture)
+
+BOOST_AUTO_TEST_CASE(GoodSubscription) {
+  BOOST_CHECK(Validate());
+}
+
+BOOST_AUTO_TEST_CASE(EmptyLines) {
+  for (auto const& s : subscription)
+    lines.push_back(std::string("\n\n" + s + "\n\n"));
+  BOOST_CHECK(Validate());
+}
+
+BOOST_AUTO_TEST_CASE(WhiteSpaces) {
+  for (auto const& s : subscription)
+    lines.push_back(std::string(" " + s + " "));
+  BOOST_CHECK(Validate());
+}
+
+BOOST_AUTO_TEST_CASE(BadHosts) {
+  for (std::size_t i = 0; i < subscription.size(); i++)
+    lines.push_back("x9a0f3;21n,.mas=AQZGLAMpI9Q0l0kmMj1vpJJYK3CjLp~fE3MfvE-e7KMKjI5cPOH6EN8m794uHJ6b09qM8mb9VEv1lVLEov~usVliTSXCSHuRBOCIwIOuDNU0AbVa4BpIx~2sU4TxKhoaA3zQ6VzINoduTdR2IJhPvI5xzezp7dR21CEQGGTbenDslXeQ4iLHFA2~bzp1f7etSl9T2W9RID-KH78sRQmzWnv7dbhNodMbpO6xsf1vENf6bMRzqD5vgHEHZu2aSoNuPyYxDU1eM6--61b2xp9mt1k3ud-5WvPVg89RaU9ugU5cxaHgR927lHMCAEU2Ax~zUb3DbrvgQBOTHnJEx2Fp7pOK~PnP6ylkYKQMfLROosLDXinxOoSKP0UYCh2WgIUPwE7WzJH3PiJVF0~WZ1dZ9mg00c~gzLgmkOxe1NpFRNg6XzoARivNVB5NuWqNxr5WKWMLBGQ9YHvHO1OHhUJTowb9X90BhtHnLK2AHwO6fV-iHWxRJyDabhSMj1kuYpVUBQAEAAcAAA==");
+  BOOST_CHECK(Validate());
+}
+
+BOOST_AUTO_TEST_CASE(BadAddresses) {
+  for (std::size_t i = 0; i < subscription.size(); i++)
+    lines.push_back("anonimal.i2p=123456");
+  BOOST_CHECK(Validate());
+}
+
+BOOST_AUTO_TEST_CASE(GarbageLines) {
+  for (std::size_t i = 0; i < subscription.size(); i++) {
+    std::array<std::uint8_t, 100> rand;
+    kovri::core::RandBytes(rand.data(), rand.size());
+    const std::string line(std::begin(rand), std::end(rand));
+    lines.push_back(line);
+  }
+  BOOST_CHECK(Validate());
+}
+
+BOOST_AUTO_TEST_CASE(LongLines) {
+  for (std::size_t i = 0; i < subscription.size(); i++) {
+    const std::string line(book.SubscriptionLine + 1000, 'A');
+    lines.push_back(line);
+  }
+  BOOST_CHECK(Validate());
+}
+
+BOOST_AUTO_TEST_CASE(PGPClearSign) {
+  const std::string line =
+    "-----BEGIN PGP SIGNED MESSAGE-----"
+    "Hash: SHA256"
+    ""
+    "anonimal.i2p=AQZGLAMpI9Q0l0kmMj1vpJJYK3CjLp~fE3MfvE-e7KMKjI5cPOH6EN8m794uHJ6b09qM8mb9VEv1lVLEov~usVliTSXCSHuRBOCIwIOuDNU0AbVa4BpIx~2sU4TxKhoaA3zQ6VzINoduTdR2IJhPvI5xzezp7dR21CEQGGTbenDslXeQ4iLHFA2~bzp1f7etSl9T2W9RID-KH78sRQmzWnv7dbhNodMbpO6xsf1vENf6bMRzqD5vgHEHZu2aSoNuPyYxDU1eM6--61b2xp9mt1k3ud-5WvPVg89RaU9ugU5cxaHgR927lHMCAEU2Ax~zUb3DbrvgQBOTHnJEx2Fp7pOK~PnP6ylkYKQMfLROosLDXinxOoSKP0UYCh2WgIUPwE7WzJH3PiJVF0~WZ1dZ9mg00c~gzLgmkOxe1NpFRNg6XzoARivNVB5NuWqNxr5WKWMLBGQ9YHvHO1OHhUJTowb9X90BhtHnLK2AHwO6fV-iHWxRJyDabhSMj1kuYpVUBQAEAAcAAA=="
+    "-----BEGIN PGP SIGNATURE-----"
+    ""
+    "iQIzBAEBCAAdFiEEEhhics1I4lOeLdKbZqduz5FECfEFAlhYd6MACgkQZqduz5FE"
+    "CfFp9RAAm7fzWX/3ojOl+sF9attDNub7FY0kuhgOzhjiUqnoN7lokaHI+EMGtFNz"
+    "Mkf48lDwqNWtOQ6aaJSFBUzQC+fn2OrTinfVfIxHvDak8NnMzl1GZh51iZSusaYn"
+    "SsJ9pRhxwVIK2VaxlN08UKjMAhlTmxYOKsotEGUCj1hcuIXSeMVpK4Xt4qjJNic4"
+    "4i+9t5Lmcz1ZVDwfKIcvvNlW/qXNxyYVIXEnkHDkhPozmgUcPYSjoBk5TKmrAama"
+    "BtvYicxdaioHVH01KRi6/tPCZ7KgwvgB/LAIIMLZVsh3dVt7K9uoKZKTvRHVLJiq"
+    "25TegnZgVYtoHiW088Y9Yqj72NpQO8KvWD8rSnj4UymMZgRclEX1m5Q2ke8EohDZ"
+    "20cBPSOpMg5P6c0CsLXlGw8DwyeYYlA45va4BxAUGAIKTaC3aw6+T7C6pCs4Xv7G"
+    "ufUzlGclVhwla5SAAvgL2U4ux8zxCg01PdOdXR8gRLKzfoabCsSuTFLWQB+wpqLu"
+    "fVcY4VzH8FlT18ZWoJYgMR2Z1NHGWpalhm24cc8XCXPv3wfAsSdAU1PTMyG7Lfna"
+    "8Z7DUkJcRcOnmfW+zB/NO4LoffOMXdQZTtM8K77sDTqKLGUaPoARwgMgGkfBOPC8"
+    "eHM9ZXgdxRBeOQhEaVEre4n3+2NYDzB9rfZmGCRsm9lr6MKcrTE="
+    "=P8Ug"
+    "-----END PGP SIGNATURE-----";
+  lines.push_back(line);
+  BOOST_CHECK(Validate());
+}
+
+// TODO(unassigned): more cases?
+
+BOOST_AUTO_TEST_SUITE_END() }  // TODO(unassigned): why is this stray brace needed?

src/client/address_book/impl.h
@@ -126,7 +126,6 @@ class AddressBook : public AddressBookDefaults {
 
   /// @brief Creates new address book filesystem storage instance
   /// @return Unique pointer to address book filesystem storage instance
-  //std::unique_ptr<AddressBookStorage> GetNewStorageInstance() {
   std::unique_ptr<AddressBookStorage> GetNewStorageInstance() {
     return std::make_unique<AddressBookStorage>();
   }
@@ -147,9 +146,8 @@ class AddressBook : public AddressBookDefaults {
   /// @brief Validates subscription, saves hosts to file
   /// @param stream Stream to process
   /// @return Vector of paired hostname to identity
-  const std::vector<std::pair<std::string, kovri::core::IdentityEx>>
-  ValidateSubscription(
-      std::istream& stream);
+  const std::map<std::string, kovri::core::IdentityEx>
+  ValidateSubscription(std::istream& stream);
 
   /// @brief Sets the download state as complete and resets timer as needed
   /// @details Resets update timer according to the state of completed download

src/client/address_book/storage.h
@@ -85,6 +85,13 @@ struct AddressBookDefaults {
   std::string GetDefaultAddressesFilename() const {
     return "addresses.csv";
   }
+
+  /// @enum AddressBookSize
+  enum AddressBookSize : std::uint16_t {
+    /// @brief Line in subscription file
+    /// @Note Arbitrary amount, should never need to exceed this amount
+    SubscriptionLine = 600,
+  };
 };
 
 /// @class AddressBookStorage

tests/unit_tests/CMakeLists.txt
@@ -1,4 +1,5 @@
 set(TESTS_CLIENT
+  "client/address_book/impl.cc"
   "client/reseed.cc"
   "client/util/http.cc"
   "client/util/parse.cc"

commit 1fb2334b0b143d49899a5234d8fd52c9f865ef7d

AddressBook: fix host-saving bug introduced in 5062d70
- Previous local variable naming lead to setting the wrong key
  (should be hostname, not line) when returning valid addresses
- Fixes run-time and unit-test

References #305

src/client/address_book/impl.cc
@@ -374,7 +374,7 @@ AddressBook::ValidateSubscription(std::istream& stream) {
           LogPrint(eLogWarn, "AddressBook: malformed address, skipping");
           continue;
         }
-        addresses[hostname] = ident;  // Host is valid, save
+        addresses[name] = ident;  // Host is valid, save
       }
     }
   } catch (const std::exception& ex) {

tests/unit_tests/client/address_book/impl.cc
@@ -67,7 +67,7 @@ struct SubscriptionFixture {
     // Validate
     std::vector<std::string> validated;
     for (auto const& v : Validate(lines))
-      validated.push_back(v.first);
+      validated.push_back(v.first + "=" + v.second.ToBase64());
     return validated != subscription ? false : true;
   }
 
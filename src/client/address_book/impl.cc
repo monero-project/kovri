monero-project/kovri

commit e7f6c5907e487950e9af31af97bae3c090558f02

AddressBook: use boost to trim whitespace
References #512 #305

src/client/address_book/impl.cc
@@ -32,6 +32,7 @@
 
 #include "client/address_book/impl.h"
 
+#include <boost/algorithm/string/trim.hpp>
 #include <boost/asio.hpp>
 
 #include <array>
@@ -357,14 +358,8 @@ AddressBook::ValidateSubscription(std::istream& stream) {
       // Skip empty / too large lines
       if (line.empty() || line.size() > AddressBookSize::SubscriptionLine)
         continue;
-      // TODO(anonimal): Boost refactor
-      // Clear whitespace before and after host (on the line)
-      line.erase(
-          std::remove_if(
-              line.begin(),
-              line.end(),
-              [](char whitespace) { return std::isspace(whitespace); }),
-          line.end());
+      // Trim whitespace before and after line
+      boost::trim(line);
       // Parse Hostname=Base64Address from line
       kovri::core::IdentityEx ident;
       std::size_t pos = line.find('=');

commit 2d99c7c334634b919b62768d04175a735b43de3b

AddressBook: fix subscription host writing bug
Introduced in #512, only the hostname was written to subscription file after a new download
(proper pairing is currently hostname=base64)

Because we package the default subscription, this issue would only have been noticeable if
an upstream publisher changed their subscription.

References #305

src/client/address_book/impl.cc
@@ -314,8 +314,8 @@ bool AddressBook::SaveSubscription(
       for (auto const& address : addresses) {
         const std::string& host = address.first;
         const auto& ident = address.second;
-        // Write/overwrite to subscription file
-        file << host << '\n';
+        // Write/overwrite Hostname=Base64Address pairing to subscription file
+        file << host << "=" << ident.ToBase64() << '\n';  // TODO(anonimal): this is not optimal, especially for large subscriptions
         // Add to address book
         m_Storage->AddAddress(ident);
         m_Addresses[host] = ident.GetIdentHash();  // TODO(anonimal): setter?
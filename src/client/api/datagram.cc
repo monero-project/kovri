monero-project/kovri

commit ca04ff4421eb0652f687b2554ec1ca4aa3b4f849

Referencing #755
Removes signatures no longer needed for verifying RouterIdentity
signatures.

Keeps ed25519 and Elgamal+SHA512 for current (0.9.16+) I2P routers.

src/core/crypto/impl/cryptopp/signature.cc
@@ -57,423 +57,6 @@ namespace core {
 
 /**
  *
- * DSA
- *
- */
-
-/// @class DSAVerifierImpl
-/// @brief DSA verifier implementation
-class DSAVerifier::DSAVerifierImpl {
- public:
-  DSAVerifierImpl(
-      const std::uint8_t* signing_key) {
-    m_PublicKey.Initialize(
-        dsap,
-        dsaq,
-        dsag,
-        CryptoPP::Integer(
-            signing_key,
-            DSA_PUBLIC_KEY_LENGTH));
-  }
-
-  bool Verify(
-      const std::uint8_t* buf,
-      std::size_t len,
-      const std::uint8_t* signature) const {
-    CryptoPP::DSA::Verifier verifier(m_PublicKey);
-    return verifier.VerifyMessage(buf, len, signature, DSA_SIGNATURE_LENGTH);
-  }
-
- private:
-  CryptoPP::DSA::PublicKey m_PublicKey;
-};
-
-DSAVerifier::DSAVerifier(
-    const std::uint8_t* signing_key)
-    : m_DSAVerifierPimpl(
-          std::make_unique<DSAVerifierImpl>(signing_key)) {}
-
-DSAVerifier::~DSAVerifier() {}
-
-bool DSAVerifier::Verify(
-    const std::uint8_t* buf,
-    std::size_t len,
-    const std::uint8_t* signature) const {
-  return m_DSAVerifierPimpl->Verify(buf, len, signature);
-}
-
-/// @class DSASignerImpl
-/// @brief DSA signing implementation
-class DSASigner::DSASignerImpl {
- public:
-  DSASignerImpl(
-      const std::uint8_t* private_signing_key) {
-    m_PrivateKey.Initialize(
-        dsap,
-        dsaq,
-        dsag,
-        CryptoPP::Integer(
-            private_signing_key,
-            DSA_PRIVATE_KEY_LENGTH));
-  }
-
-  void Sign(
-      const std::uint8_t* buf,
-      std::size_t len,
-      std::uint8_t* signature) const {
-    CryptoPP::DSA::Signer signer(m_PrivateKey);
-    CryptoPP::AutoSeededRandomPool prng;
-    signer.SignMessage(prng, buf, len, signature);
-  }
-
- private:
-  CryptoPP::DSA::PrivateKey m_PrivateKey;
-};
-
-DSASigner::DSASigner(
-    const std::uint8_t* private_signing_key)
-    : m_DSASignerPimpl(
-          std::make_unique<DSASignerImpl>(private_signing_key)) {}
-
-DSASigner::~DSASigner() {}
-
-void DSASigner::Sign(
-    const std::uint8_t* buf,
-    std::size_t len,
-    std::uint8_t* signature) const {
-  m_DSASignerPimpl->Sign(buf, len, signature);
-}
-
-// Create keys
-void CreateDSARandomKeys(
-    std::uint8_t* private_signing_key,
-    std::uint8_t* public_signing_key) {
-  std::array<std::uint8_t, DSA_PRIVATE_KEY_LENGTH> key_buf;
-  CryptoPP::Integer dsax;
-  do {
-    kovri::core::RandBytes(key_buf.data(), DSA_PRIVATE_KEY_LENGTH);
-    dsax = CryptoPP::Integer(key_buf.data(), DSA_PRIVATE_KEY_LENGTH);
-  } while (dsax.IsZero() || dsax >= dsaq);
-  CryptoPP::DSA::PrivateKey private_key;
-  CryptoPP::DSA::PublicKey public_key;
-  private_key.Initialize(dsap, dsaq, dsag, dsax);
-  private_key.MakePublicKey(public_key);
-  private_key.GetPrivateExponent().Encode(
-      private_signing_key,
-      DSA_PRIVATE_KEY_LENGTH);
-  public_key.GetPublicElement().Encode(
-      public_signing_key,
-      DSA_PUBLIC_KEY_LENGTH);
-}
-
-/**
- *
- * ECDSA
- *
- */
-
-
-/// @class ECDSAVerifier
-/// @brief ECDSA verifier base class
-template<typename Hash, std::size_t KeyLen>
-class ECDSAVerifier {
- public:
-  template<typename Curve>
-  ECDSAVerifier(
-      Curve curve,
-      const std::uint8_t* signing_key) {
-    m_PublicKey.Initialize(
-        curve,
-        CryptoPP::ECP::Point(
-            CryptoPP::Integer(
-                signing_key,
-                KeyLen / 2),
-            CryptoPP::Integer(
-                signing_key + KeyLen / 2,
-                KeyLen / 2)));
-  }
-
-  bool Verify(
-      const std::uint8_t* buf,
-      std::size_t len,
-      const std::uint8_t* signature) const {
-    typename CryptoPP::ECDSA<CryptoPP::ECP, Hash>::Verifier
-      verifier(m_PublicKey);
-    return verifier.VerifyMessage(
-        buf,
-        len,
-        signature,
-        KeyLen);  // Signature length
-  }
-
- private:
-  typename CryptoPP::ECDSA<CryptoPP::ECP, Hash>::PublicKey m_PublicKey;
-};
-
-/// @class ECDSASigner
-/// @brief ECDSA signer base class
-template<typename Hash>
-class ECDSASigner : public Signer {
- public:
-  template<typename Curve>
-  ECDSASigner(
-      Curve curve,
-      const std::uint8_t* private_signing_key,
-      std::size_t key_length) {
-    m_PrivateKey.Initialize(
-        curve,
-        CryptoPP::Integer(
-            private_signing_key,
-            key_length / 2));  // Private key length
-  }
-
-  void Sign(
-      const std::uint8_t* buf,
-      std::size_t len,
-      std::uint8_t* signature) const {
-    typename CryptoPP::ECDSA<CryptoPP::ECP, Hash>::Signer
-      signer(m_PrivateKey);
-    CryptoPP::AutoSeededRandomPool prng;
-    signer.SignMessage(prng, buf, len, signature);
-  }
-
- private:
-  typedef typename CryptoPP::ECDSA<CryptoPP::ECP, Hash>::PrivateKey
-    SignKey;
-  SignKey m_PrivateKey;
-};
-
-// Create keys
-template<typename Hash, typename Curve>
-inline void CreateECDSARandomKeys(
-    Curve curve,
-    std::size_t key_length,
-    std::uint8_t* private_signing_key,
-    std::uint8_t* public_signing_key) {
-  typename CryptoPP::ECDSA<CryptoPP::ECP, Hash>::PrivateKey
-    private_key;
-  typename CryptoPP::ECDSA<CryptoPP::ECP, Hash>::PublicKey
-    public_key;
-  CryptoPP::AutoSeededRandomPool prng;
-  private_key.Initialize(prng, curve);
-  private_key.MakePublicKey(public_key);
-  private_key.GetPrivateExponent().Encode(private_signing_key, key_length / 2);
-  auto q = public_key.GetPublicElement();
-  q.x.Encode(public_signing_key, key_length / 2);
-  q.y.Encode(public_signing_key + key_length / 2, key_length / 2);
-}
-
-/**
- *
- * ECDSAP256
- *
- */
-
-/// @class ECDSAP256VerifierImpl
-/// @brief ECDSAP256 verifier implementation
-class ECDSAP256Verifier::ECDSAP256VerifierImpl
-    : public ECDSAVerifier<CryptoPP::SHA256, ECDSAP256_KEY_LENGTH> {
- public:
-  ECDSAP256VerifierImpl(
-      const std::uint8_t* signing_key)
-      : ECDSAVerifier(
-            CryptoPP::ASN1::secp256r1(),
-            signing_key) {}
-};
-
-ECDSAP256Verifier::ECDSAP256Verifier(
-    const std::uint8_t* signing_key)
-    : m_ECDSAP256VerifierPimpl(
-          std::make_unique<ECDSAP256VerifierImpl>(signing_key)) {}
-
-ECDSAP256Verifier::~ECDSAP256Verifier() {}
-
-bool ECDSAP256Verifier::Verify(
-    const std::uint8_t* buf,
-    std::size_t len,
-    const std::uint8_t* signature) const {
-  return m_ECDSAP256VerifierPimpl->Verify(buf, len, signature);
-}
-
-/// @class ECDSAP256SignerImpl
-/// @brief ECDSAP256 signing implementation
-class ECDSAP256Signer::ECDSAP256SignerImpl
-    : public ECDSASigner<CryptoPP::SHA256> {
- public:
-  ECDSAP256SignerImpl(
-      const std::uint8_t* private_signing_key)
-      : ECDSASigner(
-            CryptoPP::ASN1::secp256r1(),
-            private_signing_key,
-            ECDSAP256_KEY_LENGTH) {}
-};
-
-ECDSAP256Signer::ECDSAP256Signer(
-    const std::uint8_t* private_signing_key)
-    : m_ECDSAP256SignerPimpl(
-          std::make_unique<ECDSAP256SignerImpl>(private_signing_key)) {}
-
-ECDSAP256Signer::~ECDSAP256Signer() {}
-
-void ECDSAP256Signer::Sign(
-    const std::uint8_t* buf,
-    std::size_t len,
-    std::uint8_t* signature) const {
-  m_ECDSAP256SignerPimpl->Sign(buf, len, signature);
-}
-
-// Create keys
-void CreateECDSAP256RandomKeys(
-    std::uint8_t* private_signing_key,
-    std::uint8_t* public_signing_key) {
-  CreateECDSARandomKeys<CryptoPP::SHA256>(
-      CryptoPP::ASN1::secp256r1(),
-      ECDSAP256_KEY_LENGTH,
-      private_signing_key,
-      public_signing_key);
-}
-
-/**
- *
- * ECDSAP384
- *
- */
-
-/// @class ECDSAP384VerifierImpl
-/// @brief ECDSAP384 verifier implementation
-class ECDSAP384Verifier::ECDSAP384VerifierImpl
-    : public ECDSAVerifier<CryptoPP::SHA384, ECDSAP384_KEY_LENGTH> {
- public:
-  ECDSAP384VerifierImpl(
-      const std::uint8_t* signing_key)
-      : ECDSAVerifier(
-            CryptoPP::ASN1::secp384r1(),
-            signing_key) {}
-};
-
-ECDSAP384Verifier::ECDSAP384Verifier(
-    const std::uint8_t* signing_key)
-    : m_ECDSAP384VerifierPimpl(
-          std::make_unique<ECDSAP384VerifierImpl>(signing_key)) {}
-
-ECDSAP384Verifier::~ECDSAP384Verifier() {}
-
-bool ECDSAP384Verifier::Verify(
-    const std::uint8_t* buf,
-    std::size_t len,
-    const std::uint8_t* signature) const {
-  return m_ECDSAP384VerifierPimpl->Verify(buf, len, signature);
-}
-
-/// @class ECDSAP384SignerImpl
-/// @brief ECDSAP384 signing implementation
-class ECDSAP384Signer::ECDSAP384SignerImpl
-    : public ECDSASigner<CryptoPP::SHA384> {
- public:
-  ECDSAP384SignerImpl(
-      const std::uint8_t* private_signing_key)
-      : ECDSASigner(
-            CryptoPP::ASN1::secp384r1(),
-            private_signing_key,
-            ECDSAP384_KEY_LENGTH) {}
-};
-
-ECDSAP384Signer::ECDSAP384Signer(
-    const std::uint8_t* private_signing_key)
-    : m_ECDSAP384SignerPimpl(
-          std::make_unique<ECDSAP384SignerImpl>(private_signing_key)) {}
-
-ECDSAP384Signer::~ECDSAP384Signer() {}
-
-void ECDSAP384Signer::Sign(
-    const std::uint8_t* buf,
-    std::size_t len,
-    std::uint8_t * signature) const {
-  m_ECDSAP384SignerPimpl->Sign(buf, len, signature);
-}
-
-// Create keys
-void CreateECDSAP384RandomKeys(
-    std::uint8_t* private_signing_key,
-    std::uint8_t* public_signing_key) {
-  CreateECDSARandomKeys<CryptoPP::SHA384>(
-      CryptoPP::ASN1::secp384r1(),
-      ECDSAP384_KEY_LENGTH,
-      private_signing_key,
-      public_signing_key);
-}
-
-/**
- *
- * ECDSAP521
- *
- */
-/// @class ECDSAP521VerifierImpl
-/// @brief ECDSAP521 verifier implementation
-class ECDSAP521Verifier::ECDSAP521VerifierImpl
-    : public ECDSAVerifier<CryptoPP::SHA512, ECDSAP521_KEY_LENGTH> {
- public:
-  ECDSAP521VerifierImpl(
-      const std::uint8_t* signing_key)
-      : ECDSAVerifier(
-            CryptoPP::ASN1::secp521r1(),
-            signing_key) {}
-};
-
-ECDSAP521Verifier::ECDSAP521Verifier(
-    const std::uint8_t* signing_key)
-    : m_ECDSAP521VerifierPimpl(
-          std::make_unique<ECDSAP521VerifierImpl>(signing_key)) {}
-
-ECDSAP521Verifier::~ECDSAP521Verifier() {}
-
-bool ECDSAP521Verifier::Verify(
-    const std::uint8_t* buf,
-    std::size_t len,
-    const std::uint8_t* signature) const {
-  return m_ECDSAP521VerifierPimpl->Verify(buf, len, signature);
-}
-
-/// @class ECDSAP521SignerImpl
-/// @brief ECDSAP521 signing implementation
-class ECDSAP521Signer::ECDSAP521SignerImpl
-    : public ECDSASigner<CryptoPP::SHA512> {
- public:
-  ECDSAP521SignerImpl(
-      const std::uint8_t* private_signing_key)
-      : ECDSASigner(
-            CryptoPP::ASN1::secp521r1(),
-            private_signing_key,
-            ECDSAP521_KEY_LENGTH) {}
-};
-
-ECDSAP521Signer::ECDSAP521Signer(
-    const std::uint8_t* private_signing_key)
-    : m_ECDSAP521SignerPimpl(
-          std::make_unique<ECDSAP521SignerImpl>(private_signing_key)) {}
-
-ECDSAP521Signer::~ECDSAP521Signer() {}
-
-void ECDSAP521Signer::Sign(
-    const std::uint8_t* buf,
-    std::size_t len,
-    std::uint8_t* signature) const {
-  m_ECDSAP521SignerPimpl->Sign(buf, len, signature);
-}
-
-// Create keys
-void CreateECDSAP521RandomKeys(
-    std::uint8_t* private_signing_key,
-    std::uint8_t* public_signing_key) {
-  CreateECDSARandomKeys<CryptoPP::SHA512>(
-      CryptoPP::ASN1::secp521r1(),
-      ECDSAP521_KEY_LENGTH,
-      private_signing_key,
-      public_signing_key);
-}
-
-/**
- *
  * RSA
  *
  */
@@ -509,37 +92,6 @@ class RSAVerifier {
   CryptoPP::RSA::PublicKey m_PublicKey;
 };
 
-/// @class RSASigner
-/// @brief RSA signing base class
-template<typename Hash>
-class RSASigner {
- public:
-  RSASigner(
-      const std::uint8_t* private_signing_key,
-      std::size_t key_length) {
-    m_PrivateKey.Initialize(
-        CryptoPP::Integer(
-            private_signing_key,
-            key_length / 2),
-            rsae,
-            CryptoPP::Integer(
-                private_signing_key + key_length / 2,
-                key_length / 2));
-  }
-
-  void Sign(
-      const std::uint8_t* buf,
-      std::size_t len,
-      std::uint8_t* signature) const {
-    CryptoPP::AutoSeededRandomPool prng;
-    typename CryptoPP::RSASS<CryptoPP::PKCS1v15, Hash>::Signer
-    signer(m_PrivateKey);
-    signer.SignMessage(prng, buf, len, signature);
-  }
-
- private:
-  CryptoPP::RSA::PrivateKey m_PrivateKey;
-};
 
 // Create keys
 void CreateRSARandomKeys(
@@ -561,114 +113,6 @@ void CreateRSARandomKeys(
   private_key.GetModulus().Encode(
       public_signing_key,
       public_key_length);
-}
-
-/**
- *
- * RSASHA2562048
- *
- */
-
-/// @class RSASHA2562048VerifierImpl
-/// @brief RSASHA2562048 verifier implementation
-class RSASHA2562048Verifier::RSASHA2562048VerifierImpl
-    : public RSAVerifier<CryptoPP::SHA256, RSASHA2562048_KEY_LENGTH> {
- public:
-  explicit RSASHA2562048VerifierImpl(
-      const std::uint8_t* public_key)
-      : RSAVerifier<CryptoPP::SHA256, RSASHA2562048_KEY_LENGTH>(public_key) {}
-};
-
-RSASHA2562048Verifier::RSASHA2562048Verifier(
-    const std::uint8_t* pubKey)
-    : m_RSASHA2562048VerifierPimpl(
-          std::make_unique<RSASHA2562048VerifierImpl>(pubKey)) {}
-
-RSASHA2562048Verifier::~RSASHA2562048Verifier() {}
-
-bool RSASHA2562048Verifier::Verify(
-    const std::uint8_t* buf,
-    std::size_t len,
-    const std::uint8_t* signature) const {
-  return m_RSASHA2562048VerifierPimpl->Verify(buf, len, signature);
-}
-
-/// @class RSASHA2562048SignerImpl
-/// @brief RSASHA2562048 signing implementation
-class RSASHA2562048Signer::RSASHA2562048SignerImpl
-    : public RSASigner<CryptoPP::SHA256> {
- public:
-  RSASHA2562048SignerImpl(
-      const std::uint8_t* privkey)
-      : RSASigner<CryptoPP::SHA256>(privkey, RSASHA2562048_KEY_LENGTH * 2) {}
-};
-
-RSASHA2562048Signer::RSASHA2562048Signer(
-    const std::uint8_t* private_key)
-    : m_RSASHA2562048SignerPimpl(
-          std::make_unique<RSASHA2562048SignerImpl>(private_key)) {}
-
-RSASHA2562048Signer::~RSASHA2562048Signer() {}
-
-void RSASHA2562048Signer::Sign(
-    const std::uint8_t* buf,
-    std::size_t len,
-    std::uint8_t* signature) const {
-  m_RSASHA2562048SignerPimpl->Sign(buf, len, signature);
-}
-
-/**
- *
- * RSASHA3843072
- *
- */
-
-/// @class RSASHA3843072VerifierImpl
-/// @brief RSASHA3843072 verifier implementation
-class RSASHA3843072Verifier::RSASHA3843072VerifierImpl
-    : public RSAVerifier<CryptoPP::SHA384, RSASHA3843072_KEY_LENGTH> {
- public:
-  explicit RSASHA3843072VerifierImpl(
-      const std::uint8_t* public_key)
-      : RSAVerifier<CryptoPP::SHA384, RSASHA3843072_KEY_LENGTH>(public_key) {}
-};
-
-RSASHA3843072Verifier::RSASHA3843072Verifier(
-    const std::uint8_t* pubKey)
-    : m_RSASHA3843072VerifierPimpl(
-          std::make_unique<RSASHA3843072VerifierImpl>(pubKey)) {}
-
-RSASHA3843072Verifier::~RSASHA3843072Verifier() {}
-
-bool RSASHA3843072Verifier::Verify(
-    const std::uint8_t* buf,
-    std::size_t len,
-    const std::uint8_t* signature) const {
-  return m_RSASHA3843072VerifierPimpl->Verify(buf, len, signature);
-}
-
-/// @class RSASHA3843072SignerImpl
-/// @brief RSASHA3843072 signing implementation
-class RSASHA3843072Signer::RSASHA3843072SignerImpl
-    : public RSASigner<CryptoPP::SHA384> {
- public:
-  RSASHA3843072SignerImpl(
-      const std::uint8_t* privkey)
-      : RSASigner<CryptoPP::SHA384>(privkey, RSASHA3843072_KEY_LENGTH * 2) {}
-};
-
-RSASHA3843072Signer::RSASHA3843072Signer(
-    const std::uint8_t* private_key)
-    : m_RSASHA3843072SignerPimpl(
-          std::make_unique<RSASHA3843072SignerImpl>(private_key)) {}
-
-RSASHA3843072Signer::~RSASHA3843072Signer() {}
-
-void RSASHA3843072Signer::Sign(
-    const std::uint8_t* buf,
-    std::size_t len,
-    std::uint8_t* signature) const {
-  m_RSASHA3843072SignerPimpl->Sign(buf, len, signature);
 }
 
 /**
@@ -699,30 +143,6 @@ bool RSASHA5124096Verifier::Verify(
     std::size_t len,
     const std::uint8_t* signature) const {
   return m_RSASHA5124096VerifierPimpl->Verify(buf, len, signature);
-}
-
-/// @class RSASHA5124096SignerImpl
-/// @brief RSASHA5124096 signing implementation
-class RSASHA5124096Signer::RSASHA5124096SignerImpl
-    : public RSASigner<CryptoPP::SHA512> {
- public:
-  RSASHA5124096SignerImpl(
-      const std::uint8_t* privkey)
-      : RSASigner<CryptoPP::SHA512>(privkey, RSASHA5124096_KEY_LENGTH * 2) {}
-};
-
-RSASHA5124096Signer::RSASHA5124096Signer(
-    const std::uint8_t* private_key)
-    : m_RSASHA5124096SignerPimpl(
-          std::make_unique<RSASHA5124096SignerImpl>(private_key)) {}
-
-RSASHA5124096Signer::~RSASHA5124096Signer() {}
-
-void RSASHA5124096Signer::Sign(
-    const std::uint8_t* buf,
-    std::size_t len,
-    std::uint8_t* signature) const {
-  m_RSASHA5124096SignerPimpl->Sign(buf, len, signature);
 }
 
 /**

commit 67885bec370ec063042893f9a5c543156aa7d581

Crypto: finish removing DSA/ECDSA from ca04ff4
This does not fix the broken functionality caused by ca04ff4.

See #755 for details.

src/client/api/datagram.cc
@@ -114,14 +114,7 @@ void DatagramDestination::SendDatagramTo(
     std::uint8_t* buf1 = signature + signature_len;
     std::size_t header_len = identity_len + signature_len;
     memcpy(buf1, payload, len);
-    if (m_Owner.GetIdentity().GetSigningKeyType()
-        == kovri::core::SIGNING_KEY_TYPE_DSA_SHA1) {
-      std::uint8_t hash[32];
-      kovri::core::SHA256().CalculateDigest(hash, buf1, len);
-      m_Owner.Sign(hash, 32, signature);
-    } else {
-      m_Owner.Sign(buf1, len, signature);
-    }
+    m_Owner.Sign(buf1, len, signature);
     std::unique_ptr<kovri::core::I2NPMessage> msg
         = CreateDataMessage(buf, len + header_len, from_port, to_port);
     std::shared_ptr<const kovri::core::LeaseSet> remote
@@ -192,16 +185,7 @@ void DatagramDestination::HandleDatagram(
     std::size_t identity_len = identity.FromBuffer(buf, len);
     const std::uint8_t* signature = buf + identity_len;
     std::size_t header_len = identity_len + identity.GetSignatureLen();
-    bool verified = false;
-    if (identity.GetSigningKeyType() == kovri::core::SIGNING_KEY_TYPE_DSA_SHA1) {
-      std::uint8_t hash[32];
-      kovri::core::SHA256().CalculateDigest(hash, buf + header_len, len - header_len);
-      verified = identity.Verify(hash, 32, signature);
-    } else {
-      verified =
-        identity.Verify(buf + header_len, len - header_len, signature);
-    }
-    if (verified) {
+    if (identity.Verify(buf + header_len, len - header_len, signature)) {
       auto it = m_ReceiversByPorts.find(to_port);
       if (it != m_ReceiversByPorts.end())
           it->second(

src/core/crypto/impl/cryptopp/crypto_const.cc
@@ -56,40 +56,10 @@ const std::uint8_t elgp_[256]= {
   0x15, 0x72, 0x8E, 0x5A, 0x8A, 0xAC, 0xAA, 0x68, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
 };
 
-const std::uint8_t dsap_[128]= {
-  0x9c, 0x05, 0xb2, 0xaa, 0x96, 0x0d, 0x9b, 0x97, 0xb8, 0x93, 0x19, 0x63, 0xc9, 0xcc, 0x9e, 0x8c,
-  0x30, 0x26, 0xe9, 0xb8, 0xed, 0x92, 0xfa, 0xd0, 0xa6, 0x9c, 0xc8, 0x86, 0xd5, 0xbf, 0x80, 0x15,
-  0xfc, 0xad, 0xae, 0x31, 0xa0, 0xad, 0x18, 0xfa, 0xb3, 0xf0, 0x1b, 0x00, 0xa3, 0x58, 0xde, 0x23,
-  0x76, 0x55, 0xc4, 0x96, 0x4a, 0xfa, 0xa2, 0xb3, 0x37, 0xe9, 0x6a, 0xd3, 0x16, 0xb9, 0xfb, 0x1c,
-  0xc5, 0x64, 0xb5, 0xae, 0xc5, 0xb6, 0x9a, 0x9f, 0xf6, 0xc3, 0xe4, 0x54, 0x87, 0x07, 0xfe, 0xf8,
-  0x50, 0x3d, 0x91, 0xdd, 0x86, 0x02, 0xe8, 0x67, 0xe6, 0xd3, 0x5d, 0x22, 0x35, 0xc1, 0x86, 0x9c,
-  0xe2, 0x47, 0x9c, 0x3b, 0x9d, 0x54, 0x01, 0xde, 0x04, 0xe0, 0x72, 0x7f, 0xb3, 0x3d, 0x65, 0x11,
-  0x28, 0x5d, 0x4c, 0xf2, 0x95, 0x38, 0xd9, 0xe3, 0xb6, 0x05, 0x1f, 0x5b, 0x22, 0xcc, 0x1c, 0x93
-};
-
-const std::uint8_t dsaq_[20]= {
-  0xa5, 0xdf, 0xc2, 0x8f, 0xef, 0x4c, 0xa1, 0xe2, 0x86, 0x74, 0x4c, 0xd8, 0xee, 0xd9, 0xd2, 0x9d,
-  0x68, 0x40, 0x46, 0xb7
-};
-
-const std::uint8_t dsag_[128]= {
-  0x0c, 0x1f, 0x4d, 0x27, 0xd4, 0x00, 0x93, 0xb4, 0x29, 0xe9, 0x62, 0xd7, 0x22, 0x38, 0x24, 0xe0,
-  0xbb, 0xc4, 0x7e, 0x7c, 0x83, 0x2a, 0x39, 0x23, 0x6f, 0xc6, 0x83, 0xaf, 0x84, 0x88, 0x95, 0x81,
-  0x07, 0x5f, 0xf9, 0x08, 0x2e, 0xd3, 0x23, 0x53, 0xd4, 0x37, 0x4d, 0x73, 0x01, 0xcd, 0xa1, 0xd2,
-  0x3c, 0x43, 0x1f, 0x46, 0x98, 0x59, 0x9d, 0xda, 0x02, 0x45, 0x18, 0x24, 0xff, 0x36, 0x97, 0x52,
-  0x59, 0x36, 0x47, 0xcc, 0x3d, 0xdc, 0x19, 0x7d, 0xe9, 0x85, 0xe4, 0x3d, 0x13, 0x6c, 0xdc, 0xfc,
-  0x6b, 0xd5, 0x40, 0x9c, 0xd2, 0xf4, 0x50, 0x82, 0x11, 0x42, 0xa5, 0xe6, 0xf8, 0xeb, 0x1c, 0x3a,
-  0xb5, 0xd0, 0x48, 0x4b, 0x81, 0x29, 0xfc, 0xf1, 0x7b, 0xce, 0x4f, 0x7f, 0x33, 0x32, 0x1c, 0x3c,
-  0xb3, 0xdb, 0xb1, 0x4a, 0x90, 0x5e, 0x7b, 0x2b, 0x3e, 0x93, 0xbe, 0x47, 0x08, 0xcb, 0xcc, 0x82
-};
-
 const CryptoConstants& GetCryptoConstants() {
   static CryptoConstants cryptoConstants = {
     CryptoPP::Integer(elgp_, 256),   // elgp
     CryptoPP::Integer(2),            // elgg
-    CryptoPP::Integer(dsap_, 128),   // dsap
-    CryptoPP::Integer(dsaq_, 20),    // dsaq
-    CryptoPP::Integer(dsag_, 128)    // dsag
   };
   return cryptoConstants;
 }

src/core/crypto/impl/cryptopp/crypto_const.h
@@ -42,11 +42,6 @@ struct CryptoConstants {
   // ElGamal/Diffie-Hellman
   const CryptoPP::Integer elgp;
   const CryptoPP::Integer elgg;
-
-  // DSA
-  const CryptoPP::Integer dsap;
-  const CryptoPP::Integer dsaq;
-  const CryptoPP::Integer dsag;
 };
 
 const CryptoConstants& GetCryptoConstants();
@@ -54,11 +49,6 @@ const CryptoConstants& GetCryptoConstants();
 // ElGamal/Diffie-Hellman
 #define elgp GetCryptoConstants().elgp
 #define elgg GetCryptoConstants().elgg
-
-// DSA
-#define dsap GetCryptoConstants().dsap
-#define dsaq GetCryptoConstants().dsaq
-#define dsag GetCryptoConstants().dsag
 
 // RSA
 const int rsae = 65537;

src/core/crypto/impl/cryptopp/signature.cc
@@ -32,12 +32,7 @@
 
 #include "core/crypto/signature.h"
 
-#include <cryptopp/asn.h>
-#include <cryptopp/dsa.h>
-#include <cryptopp/eccrypto.h>
 #include <cryptopp/integer.h>
-#include <cryptopp/oids.h>
-#include <cryptopp/osrng.h>
 #include <cryptopp/rsa.h>
 
 #include <array>

src/core/router/identity.cc
@@ -351,14 +351,14 @@ SigningKeyType IdentityEx::GetSigningKeyType() const {
   if (m_StandardIdentity.certificate.type ==
       CERTIFICATE_TYPE_KEY && m_ExtendedBuffer)
     return bufbe16toh(m_ExtendedBuffer.get());  // signing key
-  return SIGNING_KEY_TYPE_DSA_SHA1;
+  return SIGNING_KEY_TYPE_EDDSA_SHA512_ED25519;
 }
 
 CryptoKeyType IdentityEx::GetCryptoKeyType() const {
   if (m_StandardIdentity.certificate.type ==
       CERTIFICATE_TYPE_KEY && m_ExtendedBuffer)
     return bufbe16toh(m_ExtendedBuffer.get() + 2);  // crypto key
-  return CRYPTO_KEY_TYPE_ELGAMAL; 
+  return CRYPTO_KEY_TYPE_ELGAMAL;  // should always be ElGamal until specs determine otherwise
 }
 
 void IdentityEx::CreateVerifier() const  {

src/core/router/identity.h
@@ -150,11 +150,14 @@ struct Keys {
   std::uint8_t signing_key[128];
 };
 
+// TODO(unassigned): un-implemented
 const std::uint8_t CERTIFICATE_TYPE_NULL = 0;
 const std::uint8_t CERTIFICATE_TYPE_HASHCASH = 1;
 const std::uint8_t CERTIFICATE_TYPE_HIDDEN = 2;
 const std::uint8_t CERTIFICATE_TYPE_SIGNED = 3;
 const std::uint8_t CERTIFICATE_TYPE_MULTIPLE = 4;
+//
+
 const std::uint8_t CERTIFICATE_TYPE_KEY = 5;
 
 /// @brief Human readable description of this struct
@@ -192,16 +195,11 @@ Keys CreateRandomKeys();
 const std::size_t DEFAULT_IDENTITY_SIZE = sizeof(Identity);  // 387 bytes
 
 const std::uint16_t CRYPTO_KEY_TYPE_ELGAMAL = 0;
-const std::uint16_t SIGNING_KEY_TYPE_DSA_SHA1 = 0;
-const std::uint16_t SIGNING_KEY_TYPE_ECDSA_SHA256_P256 = 1;
-const std::uint16_t SIGNING_KEY_TYPE_ECDSA_SHA384_P384 = 2;
-const std::uint16_t SIGNING_KEY_TYPE_ECDSA_SHA512_P521 = 3;
 const std::uint16_t SIGNING_KEY_TYPE_RSA_SHA512_4096 = 6;
 const std::uint16_t SIGNING_KEY_TYPE_EDDSA_SHA512_ED25519 = 7;
 
-// TODO(anonimal): review/implement to bump client type to EdDSA-SHA512-Ed25519
-const std::uint16_t DEFAULT_CLIENT_SIGNING_KEY_TYPE = SIGNING_KEY_TYPE_ECDSA_SHA256_P256;
 const std::uint16_t DEFAULT_ROUTER_SIGNING_KEY_TYPE = SIGNING_KEY_TYPE_EDDSA_SHA512_ED25519;
+const std::uint16_t DEFAULT_CLIENT_SIGNING_KEY_TYPE = DEFAULT_ROUTER_SIGNING_KEY_TYPE;
 
 typedef std::uint16_t SigningKeyType;
 typedef std::uint16_t CryptoKeyType;
@@ -219,7 +217,7 @@ class IdentityEx {
   IdentityEx(
       const std::uint8_t* public_key,
       const std::uint8_t* signing_key,
-      SigningKeyType type = SIGNING_KEY_TYPE_DSA_SHA1);
+      SigningKeyType type = SIGNING_KEY_TYPE_EDDSA_SHA512_ED25519);
 
   IdentityEx(
       const std::uint8_t* buf,
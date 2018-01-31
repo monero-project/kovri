32cfb4b184a6ac2994f03c1f9cc9155d1b7a8467
Jeffrey Walton <noloader@gmail.com>
2018-01-30 22:44:20 -0500

    Unroll DecodingLookupArray for Base32 and Base64 decoders This is a time/space tradeoff, but it also provides 1-time intialization while avoiding a race. It should also clear https://github.com/weidai11/cryptopp/issues/562 and https://github.com/monero-project/kovri/pull/788 once and for all (famous last words)

--- src/core/crypto/impl/cryptopp/radix.cc
+++ src/core/crypto/impl/cryptopp/radix.cc
@@ -37,6 +37,49 @@
 #include <string>
 #include <vector>
 
+// Anonymous namespace
+namespace {
+    // Decoding table for m_Base32Alphabet
+    const int s_Base32Table[256] = {
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+        -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+    };
+
+    // Decoding table for m_Base64Alphabet
+    const int s_Base64Table[256] = {
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1,
+        52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+        -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+        -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, 63, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+    };
+};
+
 namespace kovri
 {
 namespace core
@@ -65,20 +108,8 @@ std::vector<std::uint8_t> Base32::Decode(
     const std::uint64_t len)
 {
   // Prepare decoder
-  int lookup[256];
-  CryptoPP::Base32Decoder::InitializeDecodingLookupArray(
-      lookup,
-      reinterpret_cast<const CryptoPP::byte*>(GetAlphabet().c_str()),
-      GetAlphabet().size(),
-      true);
-
-  CryptoPP::AlgorithmParameters const params(CryptoPP::MakeParameters(
-      CryptoPP::Name::DecodingLookupArray(),
-#if defined(__FreeBSD__)  // See #788
-      reinterpret_cast<const int*>(lookup), false));
-#else
-      reinterpret_cast<const int*>(lookup)));
-#endif
+  CryptoPP::AlgorithmParameters static const params(CryptoPP::MakeParameters(
+      CryptoPP::Name::DecodingLookupArray(), s_Base32Table, false));
 
   // Decode
   return Radix::Decode<CryptoPP::Base32Decoder>(params, in, len);
@@ -109,16 +140,8 @@ std::vector<std::uint8_t> Base64::Decode(
     const std::uint64_t len)
 {
   // Prepare decoder
-  int lookup[256];
-  CryptoPP::Base64Decoder::InitializeDecodingLookupArray(
-      lookup,
-      reinterpret_cast<const CryptoPP::byte*>(m_Base64Alphabet.c_str()),
-      GetAlphabet().size(),
-      false);
-
-  CryptoPP::AlgorithmParameters const params(CryptoPP::MakeParameters(
-      CryptoPP::Name::DecodingLookupArray(),
-      reinterpret_cast<const int*>(lookup)));
+  CryptoPP::AlgorithmParameters static const params(CryptoPP::MakeParameters(
+      CryptoPP::Name::DecodingLookupArray(), s_Base64Table, false));
 
   // Decode
   return Radix::Decode<CryptoPP::Base64Decoder>(params, in, len);

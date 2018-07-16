f4b2e7a609ff04cc7502bfa0c7fad92d9ab67d52
anonimal <anonimal@getmonero.org>
2018-07-16 04:38:18 +0000

    Crypto: implement constant-time comparator
    Uses Crypto++. See important notes in declaration and in #895.

---
+++ src/core/crypto/util/misc.h
@@ -0,0 +1,58 @@
+/**                                                                                           //
+ * Copyright (c) 2015-2018, The Kovri I2P Router Project                                      //
+ *                                                                                            //
+ * All rights reserved.                                                                       //
+ *                                                                                            //
+ * Redistribution and use in source and binary forms, with or without modification, are       //
+ * permitted provided that the following conditions are met:                                  //
+ *                                                                                            //
+ * 1. Redistributions of source code must retain the above copyright notice, this list of     //
+ *    conditions and the following disclaimer.                                                //
+ *                                                                                            //
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list     //
+ *    of conditions and the following disclaimer in the documentation and/or other            //
+ *    materials provided with the distribution.                                               //
+ *                                                                                            //
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be       //
+ *    used to endorse or promote products derived from this software without specific         //
+ *    prior written permission.                                                               //
+ *                                                                                            //
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY        //
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    //
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL     //
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,       //
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,               //
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    //
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,          //
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF    //
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               //
+ */
+
+#ifndef SRC_CORE_CRYPTO_UTIL_MISC_H_
+#define SRC_CORE_CRYPTO_UTIL_MISC_H_
+
+#include <cryptopp/misc.h>
+
+namespace kovri
+{
+namespace core
+{
+/// @brief Constant-time comparison of two equally sized buffers
+/// @param buf1 First buffer
+/// @param buf2 Second buffer
+/// @param size Byte size of both buffers
+/// @return True if buffers are equal
+/// @notes From Crypto++:
+///   "The function effectively performs an XOR of the elements in two equally sized buffers
+///   and retruns a result based on the XOR operation. The function is near constant-time because
+///   CPU micro-code timings could affect the "constant-ness". Calling code is responsible for
+///   mitigating timing attacks if the buffers are not equally sized."
+bool ConstTimeCmp(
+    const std::uint8_t* buf1,
+    const std::uint8_t* buf2,
+    const std::size_t size);
+
+}  // namespace core
+}  // namespace kovri
+
+#endif  // SRC_CORE_CRYPTO_UTIL_MISC_H_
